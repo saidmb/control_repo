4cecce1dbef94a5066451f01251e6c436199b7dd
saidmb <8784942+saidmb@users.noreply.github.com>
2018-05-28 07:43:07 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a README',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner   => 'root',
-  }
+
 }
