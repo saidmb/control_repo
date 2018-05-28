3d57144630af1d46a061d532e5ab59ba5c1a1197
saidmb <8784942+saidmb@users.noreply.github.com>
2018-05-28 09:31:21 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,9 +4,11 @@ class profile::agent_nodes {
   dockeragent::node { 'db.puppet.vm': }
 }
 
+/**********
 node /^web/ { 
   include role::app_server
 }
 node /^db/ {
   include role::db_server
 }
+*********/
