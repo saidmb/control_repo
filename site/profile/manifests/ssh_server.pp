3c0674ebd5873696542b79f69951f8174d700088
saidmb <8784942+saidmb@users.noreply.github.com>
2018-05-28 11:08:27 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDHq7t/XtVrxZ5Jy5Y3tf1cNt3IqGicXXOxYiSpVQL3f9n59JC0swiHgHlILeoqpYIgyaTK0SGWpzDSatIunp4dz6b6+xfZBr9W4UVYwWvE8AHMoW9tsDujuZhE2689qhXEAQXar8laNVabdulBrEnr4TteopoWPuzvGS5vhOyyOm75RuufHK7qEJFt1fSHKqPQNVwgyouj4BItVPhQjpDAOePYf3ifUPvhWK3ARNcd0g9CI8btbnvEzV328zoykoRNzJ/DAXQiF+xiMhKCUj6hqSy0pgzgOzkHl/QMwvKK50dPmBwrks1IVjuDsIjqIBMxdvVVj7c+Y0XLPSlfhUe/',
 	}  
 }
